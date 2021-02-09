0cb8bb6088493279d394e7e2effa1dc4953948c3
Julia Machado <juliafmarx@outlook.com>
2021-02-09 10:29:10 -0300

    Adding some images

---
+++ Quatro Cantos/InicioView.swift
@@ -0,0 +1,21 @@
+//
+//  InicioView.swift
+//  Quatro Cantos
+//
+//  Created by Julia Machado on 09/02/21.
+//
+
+import SwiftUI
+
+struct InicioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InicioView_Previews: PreviewProvider {
+    static var previews: some View {
+        InicioView()
+            .previewDevice("iPhone 11")
+    }
+}
