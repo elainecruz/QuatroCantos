956e42682028a41809dcfa50230b4c4f715e08c5
Marina Savluchinske <mfs7@academy.ufpe.br>
2021-02-09 09:38:04 -0300

    Initial Commit

---
+++ Quatro Cantos/Quatro_CantosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quatro_CantosApp.swift
+//  Quatro Cantos
+//
+//  Created by Marina Savluchinske on 09/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Quatro_CantosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
