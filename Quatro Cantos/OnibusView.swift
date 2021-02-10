bed4009052621842a1a58c0c7606fe13900e40de
Julia Machado <juliafmarx@outlook.com>
2021-02-10 09:41:22 -0300

    Adicionando imagens e criando View do onibus

---
+++ Quatro Cantos/OnibusView.swift
@@ -0,0 +1,23 @@
+//
+//  OnibusView.swift
+//  Quatro Cantos
+//
+//  Created by Julia Machado on 10/02/21.
+//
+
+import SwiftUI
+
+struct OnibusView: View {
+    var body: some View {
+        VStack{
+            ZStack{
+            }
+        }
+    }
+}
+
+struct OnibusView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnibusView()
+    }
+}
