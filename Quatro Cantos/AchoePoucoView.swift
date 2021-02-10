83d909aa4b00ad22e2ab55b6729001fb7f798bc9
Elaine  Cruz <elainecruz@MacBook-Pro-de-Elaine.local>
2021-02-10 11:45:30 -0300

    Interface da view AchoePouco feita

--- Quatro Cantos/AchoePoucoView.swift
+++ Quatro Cantos/AchoePoucoView.swift
@@ -13,48 +13,74 @@ struct AchoePoucoView: View {
             Image("OnibusAchoePouco")
                 .resizable()
                 .scaledToFit()
+                .aspectRatio(1.7, contentMode: .fit)
+            
+            Spacer()
+            
             HStack{
                 
                 Image("GlitterDourado")
                     .resizable()
                     .scaledToFit()
+                    .aspectRatio(0.8, contentMode: .fit)
+                    .offset(x: 0, y: 100)
+                    //.padding(.top, 100)
 
 
                 Image("Hb20")
                     .resizable()
                     .scaledToFit()
+                    .aspectRatio(1, contentMode: .fit)
 
 
                 Image("cervejanossa")
                     .resizable()
                     .scaledToFit()
+                    .aspectRatio(0.4, contentMode: .fit)
+                    .offset(x: 0, y: 100)
 
 
             }
             Image("pocheteAmarela")
                 .resizable()
                 .scaledToFit()
+                .aspectRatio(0.9, contentMode: .fit)
+
 
 
             HStack{
                 Image("Batom")
                     .resizable()
                     .scaledToFit()
+                    .aspectRatio(0.6, contentMode: .fit)
+                    .offset(x: 0, y: -100)
+
 
 
                 Image("caipiroska")
                     .resizable()
                     .scaledToFit()
+                    .aspectRatio(0.5, contentMode: .fit)
 
 
                 Image("foiceEmartelo")
                     .resizable()
                     .scaledToFit()
+                    .aspectRatio(0.6, contentMode: .fit)
+                    .offset(x: 0, y: -100)
+
 
 
             }
             
+            Spacer()
+            
         }
+        .background(
+            Image("bgAchoePouco")
+                .resizable()
+                .ignoresSafeArea()
+        )
     }
 }
 
