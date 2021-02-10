492ae6c6dc9576da074f7692cf13fc978ac12a08
Julia Machado <juliafmarx@outlook.com>
2021-02-10 10:05:31 -0300

    Estrutura da view onibus

--- Quatro Cantos/OnibusView.swift
+++ Quatro Cantos/OnibusView.swift
@@ -11,7 +11,44 @@ struct OnibusView: View {
     var body: some View {
         VStack{
             ZStack{
+                Image("papelInicio")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .scaledToFit()
+                Text("Teu ônibus chegou!")
+                    .font(.custom("Brasilero 2018 Free", size: 48))
+                    .multilineTextAlignment(.center)
+                    .lineLimit(nil)
+                    .padding(.horizontal, 65)
             }
+            ZStack{
+                Image("balao")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .scaledToFit()
+                Text("Vai desceê!!!")
+                    .font(.custom("Brasilero 2018 Free", size: 18))
+                    .multilineTextAlignment(.center)
+                    .lineLimit(nil)
+                    .padding(.horizontal, 50)
+            }
+                VStack{
+                    Image("onibusBonfim")
+                        .resizable()
+                        .ignoresSafeArea()
+                        .scaledToFit()
+                    Spacer()
+                    Text("SIMBORA")
+                        .font(.custom("Brasilero 2018 Free", size: 48))
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                        .lineLimit(nil)
+                        .padding(.horizontal, 50)
+                }
+                .background(Image("bgVerde")
+                                .resizable()
+                                .ignoresSafeArea()
+                                .scaledToFit())
         }
     }
 }
@@ -19,5 +56,6 @@ struct OnibusView: View {
 struct OnibusView_Previews: PreviewProvider {
     static var previews: some View {
         OnibusView()
+            .previewDevice("iPhone 11")
     }
 }
